8898db83ea4b484589ce7eea3b525b27ee9b5aea
tusharsharma <tusharsharma2515@gmail.com>
2025-10-31 13:41:31 +0530

    Update package dependencies and add Android build scripts

---
+++ android/app/src/main/java/com/radha/jap/MainActivity.java
@@ -0,0 +1,5 @@
+package com.radha.jap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.radha.jap',
+  appName: 'Radha Jap',
+  webDir: 'dist'
+};
+
+export default config;
